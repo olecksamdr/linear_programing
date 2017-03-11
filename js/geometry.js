07b23b6bd581b55991553188ffc4e600f03854dd
Olecksandr <velsashok@gmail.com>
2017-03-11 13:31:10 +0200

    shade a right side and same distence in different lines

--- js/geometry.js
+++ js/geometry.js
@@ -69,19 +69,19 @@ export class Exis {
 		// кількість поділок які треба поставити на вісі Х
 		// 1 додаємо щоб врахувати 0
 		this.exisPointsCount = Math.abs(-this.minVal + Math.abs(this.maxVal)) + 1;
-		console.log('min, max', this.minVal, this.maxVal);
+		// console.log('min, max', this.minVal, this.maxVal);
 
 		// відступи між поділками
 		if (this.type == 'x') {
 			this.unit = Math.floor( this.line.width / this.exisPointsCount);
 		} else {
 			this.unit = Math.floor( this.line.height / this.exisPointsCount);
-			console.log('line height', this.line.height)
-			console.log(this.type + 'exis unit', this.unit);
+			// console.log('line height', this.line.height)
+			// console.log(this.type + 'exis unit', this.unit);
 		}
 
 		// console.log('line width', this.line.width);
-		console.log('points count', this.exisPointsCount);
+		// console.log('points count', this.exisPointsCount);
 		// console.log('unit', this.unit);
 
 	}
@@ -155,7 +155,7 @@ export class Exis {
 		} else {
 				// y exis
 			for(let i = 0; i < this.exisPointsCount; i++) {
-				console.log('yExisPointsCount', this.exisPointsCount);
+				// console.log('yExisPointsCount', this.exisPointsCount);
 
 				let margin = this.start.y + i * this.unit;
 
@@ -237,9 +237,9 @@ export class CoordinateSystem {
 		let yPointCount = Math.abs(this.minY) + Math.abs(this.maxY) + 1;
 		let xPointCount = Math.abs(this.minX) + Math.abs(this.maxX) + 1;
 
-		console.log('xPointCount', xPointCount);
-		console.log('yPointCount', yPointCount);
-		console.log('width', this.width);
+		// console.log('xPointCount', xPointCount);
+		// console.log('yPointCount', yPointCount);
+		// console.log('width', this.width);
 
 		// відступи між поділками
 		// if (this.type == 'x') {
@@ -248,7 +248,7 @@ export class CoordinateSystem {
 			this.unitY = Math.floor( (this.height - 2*this.offset) / yPointCount);
 		// }
 
-		console.log('uniyY system', this.unitY);
+		// console.log('uniyY system', this.unitY);
 
 		//  шукаємо позицію нуля на канвасі
 			let j = 0;
@@ -268,7 +268,7 @@ export class CoordinateSystem {
 
 		this.zero = new Point(x, this.trf.y(y));
 
-		console.log(this.offset, this.unitX, this.unitY, this.zero.x, this.zero.y, this.minX, this.maxX);
+		// console.log(this.offset, this.unitX, this.unitY, this.zero.x, this.zero.y, this.minX, this.maxX);
 
 		this.xExis = new Exis({
 			start: new Point(this.offset, this.zero.y),
@@ -289,7 +289,7 @@ export class CoordinateSystem {
 		this.start = new Point(this.offset, this.height - this.offset);
 		this.end = new Point(this.width - this.offset, this.offset);
 
-		console.log('graph start end', this.start, this.end);
+		// console.log('graph start end', this.start, this.end);
 	}
 
 	pushEq(eq) {
@@ -297,7 +297,7 @@ export class CoordinateSystem {
 
 		// this.ctx.lineTo(100, 100);
 		eq.draw(this);
-		console.log('drawing eq', eq);
+		// console.log('drawing eq', eq);
 	}
 
 	transformY(y) {
@@ -357,54 +357,83 @@ export class Equation {
 	_shade(graph, formX, toX, step) {
 		// довжина штриха
 		let shadeLen = 0.3;
+		let dirrection = 1;
 
-		// перевіряє мадювати штрихи зліва чи справа від лінії
-		//  Спочатку дивимося чи (0;0) не належить прямій
-		if (this.c != this.d) {
-			// не належить
-
-			console.log("CHECK");
-			// Перевірка чи для (0;0) виконується нерівність
-			console.log(!this.makeOperation(0,0));
-			if (!this.makeOperation(0,0)) {
-				// Якщо виконується будемо малювати зліва від лінії
-				shadeLen *= -1;
-				console.log("makeOperation");
-			}
-		} else {
-			// (0; 0) належить прямій. Беремо точку (0, 1)
-			// Перевірка чи для (1;0) виконується нерівність
-			if (this.makeOperation(1,0)) {
-				// Якщо виконується будемо малювати зліва від лінії
-				shadeLen *= -1;
-			}
-		}
-
-
+		// Отримуємо рівняння вигляду Ax + By + C = 0
+		// Де (A;B) кординати вектора нормалі
 		let 
 			A = this.a,
 			B = this.b,
 			C = this.c - this.d;
+		step = Math.abs(Math.sin(Math.atan(-B / A)) * step);
 
-		let x = graph.minX - 1;
+		console.log('step', Math.abs(Math.sin(Math.atan(-B / A)) * step));
+
+		// console.log("Ax + By + C = 0", A + 'x + ' + B + 'y + ' + C);
+
+		// Для того щоб 
+		let x = formX || 0;
 		let y = (-C-A*x) / B;
+		let nextX = x + step;
+		let nextY = (-C-A*nextX) / B;
 
-		let vector, vectorLen, nextY;
+		let vector, vectorLen;
 
-		for (let nextX = ++x; nextX <= graph.maxX; nextX += step) {
+		vector = new Point(nextX - x, nextY - y);
+
+		// console.log('shade: vector start', vector);
+
+		vectorLen = Math.sqrt(vector.x * vector.x + vector.y * vector.y);
+
+			// console.log('shade: vectorLen', vectorLen);
+
+		var temp = vector.x;
+		vector.x = -vector.y / vectorLen;
+		vector.y = temp / vectorLen;
+
+		// перевіряє мадювати штрихи зліва чи справа від лінії
+		//  Спочатку дивимося чи (0;0) не належить прямій
+			// не належить
+
+			// console.log("CHECK");
+			// Перевірка чи для (0;0) виконується нерівність
+			// console.log(!this.makeOperation(0,0));
+			if (this.makeOperation(x + vector.x * 0.1, x + vector.y * 0.1)) {
+				console.log(this.makeOperation(0,0));
+				// Якщо виконується будемо малювати зліва від лінії
+				dirrection = 1;
+				// console.log("makeOperation");
+			} else {
+				dirrection = -1;
+			}
+
+		if (!toX) toX = -C / A + step;
+
+		x = nextX;
+		y = nextY;
+
+		for (let nextX = x + step; nextX <= toX; nextX += step) {
 			nextY = (-C-A*nextX) / B;
 
-			vector = new Point(nextX - x, nextY - y);
-			vectorLen = Math.sqrt(vector.x * vector.x + vector.y * vector.y);
+			// console.log('shade: nextX - x', nextX - x);
+			// console.log('shade: nextY - y', nextY - y);
 
-			vector.x /= vectorLen;
-			vector.y /= vectorLen;
+			// console.log('shade: vector end', vector);
+
+			graph.ctx.moveTo(graph.transformX(x), graph.transformY(y));
+			graph.ctx.lineTo(graph.transformX(x + vector.x * shadeLen * dirrection), graph.transformY(y + vector.y * shadeLen * dirrection));
+
+			// console.log('shade: x', x);
+			// console.log('shade: y', y);
+			// console.log('shade: nextX', nextX);
+			// console.log('shade: nextY', nextY);
+			// console.log('shade: vector', vector);
+
+
+			// console.log('-----------------------');
 
 			x = nextX;
 			y = nextY;
-
-			graph.ctx.moveTo(graph.transformX(x), graph.transformY(y));
-			graph.ctx.lineTo(graph.transformX(x-vector.y*shadeLen), graph.transformY(y + vector.x * shadeLen));
 
 		}
 
@@ -438,7 +467,7 @@ export class Equation {
 		line.draw(graph.ctx);
 
 		// малює штрихи
-		this._shade(graph, graph.minX - 1, graph.maxX, 0.5);
+		this._shade(graph, graph.minX, graph.maxX, 0.2);
 
 	}
 
